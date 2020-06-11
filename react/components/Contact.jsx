74c019471d5c5b05816a26fe4bb935c558c143e8
Marko Knöbl <marko.kn@gmail.com>
2020-06-11 11:27:27 +0200

    display more contact information, don't display a summary

--- react/components/Contact.jsx
+++ react/components/Contact.jsx
@@ -3,12 +3,16 @@ const React = require("react");
 const texts = {
   heading: {
     en: "Contact",
-    de: "Kontakt"
+    de: "Kontakt",
+  },
+  address: {
+    en: "Address",
+    de: "Adresse",
   },
   phone: {
     en: "Phone",
-    de: "Telefon"
-  }
+    de: "Telefon",
+  },
 };
 
 const Contact = ({ resume, lang }) => (
@@ -19,20 +23,27 @@ const Contact = ({ resume, lang }) => (
     <div className="col-sm-9">
       <div className="row">
         <div className="col-sm-6">
+          <strong>Website</strong>
+          <div className="website">
+            <a href={resume.basics.website}>{resume.basics.website}</a>
+          </div>
+        </div>
+        <div className="col-sm-6">
           <strong>Email</strong>
           <div className="email">
             <a href={`mailto:${resume.basics.email}`}>{resume.basics.email}</a>
           </div>
         </div>
         <div className="col-sm-6">
-          <strong>{texts.heading[lang]}</strong>
-          <div className="phone">{resume.basics.phone}</div>
+          <strong>{texts.address[lang]}</strong>
+          <div className="address">
+            {resume.basics.location.address}, {resume.basics.location.city},{" "}
+            {resume.basics.location.countryCode}
+          </div>
         </div>
         <div className="col-sm-6">
-          <strong>Website</strong>
-          <div className="website">
-            <a href={resume.basics.website}>{resume.basics.website}</a>
-          </div>
+          <strong>{texts.phone[lang]}</strong>
+          <div className="phone">{resume.basics.phone}</div>
         </div>
       </div>
     </div>
